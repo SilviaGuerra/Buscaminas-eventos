49dc5399d3ef10c308b3d6db6b0373dc5abd1b0a
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-04-04 17:23:32 -0500

    Imprime numeros

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-<div class="row">
-<div class="col s1">1</div>
-<div class="col s1">2</div>
-<div class="col s1">3</div>
-<div class="col s1">4</div>
-<div class="col s1">5</div>
-<div class="col s1">6</div>
-<div class="col s1">7</div>
-<div class="col s1">8</div>
-<div class="col s1">9</div>
-<div class="col s1">10</div>
-<div class="col s1">11</div>
-<div class="col s1">12</div>
-</div>
+var numeros = document.getElementsByClassName("numero");
+for(i = 0; i < numeros.length; i++){
+  numeros[i].addEventListener("click", num);
+}
+
+function num(){
+ this.innerHTML = this.value;
+}
+
+var color = document.getElementsByClassName("color");
+for(i = 0; i < color.length; i++){
+  color[i].addEventListener("click", colour);
+}
+
+function colour(){
+  this.style.color = "blue";
+}
